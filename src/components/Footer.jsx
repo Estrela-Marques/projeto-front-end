f3f2d613130d69fafc9130db4240f28afa2ecaaa
Estela Marques <estelapfarma@gmail.com>
2023-07-27 16:13:04 -0300

    Update Footer.jsx
    Alterando rodapé

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/footer.module.css'
 export function Footer() {
   return (
     <div className={styles.footerContainer}>
-      <p className={styles.footerText}>EstelaMarques.</p>
+      <p className={styles.footerText}>☆ EstrelaMarques. </p>
     </div>
   )
 }
