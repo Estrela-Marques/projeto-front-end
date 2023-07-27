e19a3f53e5b29797c56952c158668c321ac05889
Estela Marques <estelapfarma@gmail.com>
2023-07-27 16:05:48 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/footer.module.css'
 export function Footer() {
   return (
     <div className={styles.footerContainer}>
-      <p className={styles.footerText}>PrograMaria.</p>
+      <p className={styles.footerText}>EstelaMarques.</p>
     </div>
   )
 }
